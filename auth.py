8a22579c745cc96e03dee96c649a287f7bd326b2
Nunzio Alexandro Letizia <alexandroletizia@gmail.com>
2025-09-14 17:29:02 +0200

    Fix syntax error: remove extra 'o' from start_call function

--- auth.py
+++ auth.py
@@ -75,7 +75,7 @@ def refresh_call():
             print('EMAIL or PASSWORD environment variables not found. Please set them to proceed.')
             return False
         
-        TOKEN_DICTIONARY = start_call(email=PIKTID_EMAIL, password=PIKTID_PASSWORD) o
+        TOKEN_DICTIONARY = start_call(email=PIKTID_EMAIL, password=PIKTID_PASSWORD)
         # Save the tokens in the environment variables for future use
         os.environ['ACCESS_TOKEN'] = TOKEN_DICTIONARY.get('access_token', '')
         os.environ['REFRESH_TOKEN'] = TOKEN_DICTIONARY.get('refresh_token', '')
